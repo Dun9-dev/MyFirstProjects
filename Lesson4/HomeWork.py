b1b7144f74edb6c1be0ac394a00de9b6b0c1e059
Dun9 <danilshestov35@gmail.com>
2021-08-13 15:06:45 +0500

    New print

--- Lesson4/HomeWork.py
+++ Lesson4/HomeWork.py
@@ -1,4 +1,13 @@
-pizzas = ['cheese', 'peperoni', 'meet']
-for pizza in pizzas:
-    print(f"I love pizza with {pizza}")
-print("I really love pizza")
+#pizzas = ['cheese', 'peperoni', 'meet']
+#for pizza in pizzas:
+#    print(f"I love pizza with {pizza}")
+#print("I really love pizza")
+
+#numbers = []
+#for num in range(1, 11):
+#    numbers.append(num**3)
+
+#print(numbers)
+
+kubs = [value**3 for value in range(1,11)]
+print(kubs)
--- Lesson4/Study.py
+++ Lesson4/Study.py
@@ -1,5 +1,9 @@
-magicians = ['alice', 'david', 'corolina']
-for magician in magicians:
-    print(f"{magician.title()}, that was a great trick")
-    print(f"I can`t wait to see your next trick, {magician.title()}.\n")
-print("Thank you, everyone. That was a great magic show!")
+#magicians = ['alice', 'david', 'corolina']
+#for magician in magicians:
+#   print(f"{magician.title()}, that was a great trick")
+#    print(f"I can`t wait to see your next trick, {magician.title()}.\n")
+#print("Thank you, everyone. That was a great magic show!")
+squares = []
+for value in range(1, 11):
+    squares.append(value**2)
+    print(squares)
